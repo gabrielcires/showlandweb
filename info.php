a50a66c2253796c044061029c68021ce153ed2a5
Gabriel Cires <gabrielcireslopez@gmail.com>
2018-08-17 12:57:58 +0200

    Putooooossss phpppppppppppppppsssssssss joooooooodeeeeeeeeeeeeeeeeeeeeeeeer. Coño

--- info.php
+++ info.php
@@ -1,28 +1,28 @@
 <?php 
 
-if (isset($_POST['submitM'])) {
-    $nameM = $_POST['nameM'];
-    $emailM = $_POST['emailM'];
-    $numberM = $_POST['numberM'];
-    $messageM = $_POST['messageM'];
+if (isset($_POST['submit'])) {
+    $name = $_POST['name'];
+    $email = $_POST['email'];
+    $number = $_POST['number'];
+    $files = $_FILES['files'];
     // $mailTo = "info@showlandproducciones.es";
     $mailTo = "gabrielcireslopez@gmail.com";
             
-    $formcontent="Correo: $emailM \n\nDe: $nameM \n\nTelf: $numberM\n\n$messageM";
+    $formcontent="Correo: $email \n\nNombre: $name \n\nTelf: $number \n\n"$files;
 
-    $errorEmptyM = false;
-    $errorEmailM = false;
+    $errorEmpty = false;
+    $errorEmail = false;
 
-    if (empty($nameM) || empty($emailM) || empty($messageM)) {
+    if (empty($name) || empty($email) || empty($number) || empty($files)) {
         echo "<span class='form-error'>Rellena los campos obligatorios!*</span>";
-        $errorEmptyM = true;
+        $errorEmpty = true;
 
-    } elseif (!filter_var($emailM, FILTER_VALIDATE_EMAIL)) {
+    } elseif (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
         echo "<span class='form-error'>Introduce un correo válido!</span>";
-        $errorEmailM = true;
+        $errorEmail = true;
 
     } else { 
-        mail($mailTo, $numberM, $formcontent);
+        mail($mailTo, $email, $formcontent);
         echo "<span class='form-success'>!Correo enviado! Te responderemos cuanto antes.</span>";
     }
 
@@ -32,19 +32,19 @@ if (isset($_POST['submitM'])) {
 
 <script>
 
-    $(".mailNameM, .mailEmailM, .mailNumberM").addClass("all-good");
+    $(".mailName, .mailEmail, .mailNumber").addClass("all-good");
 
-    var errorEmptyM = "<?php echo $errorEmptyM; ?>";
-    var errorEmailM = "<?php echo $errorEmailM; ?>";
+    var errorEmpty = "<?php echo $errorEmpty; ?>";
+    var errorEmail = "<?php echo $errorEmail; ?>";
 
-    if (errorEmptyM == true) {
-        $(".mailNameM, .mailEmailM, .mailNumberM").removeClass("all-good");
+    if (errorEmpty == true) {
+        $(".mailName, .mailEmail, .mailNumber").removeClass("all-good");
     }
-    if (errorEmailM == true) {
-        $(".mailEmailM").removeClass("all-good");
+    if (errorEmail == true) {
+        $(".mailEmail").removeClass("all-good");
     }
-    if (errorEmptyM == false && errorEmailM == false) {
-        $(".mailNameM, .mailEmailM, .mailNumberM").val("");
+    if (errorEmpty == false && errorEmail == false) {
+        $(".mailName, .mailEmail, .mailNumber").val("");
     }
 
 </script>
