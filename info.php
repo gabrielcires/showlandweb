e0faf4456f3baf0357ffbcd824a07079bf348ef1
Gabriel Cires <gabrielcireslopez@gmail.com>
2018-08-22 10:25:48 +0200

    Creado php de envio de archivos, falta modificar para resto de formularios

--- info.php
+++ info.php
@@ -1,33 +1,69 @@
 <?php 
 
-if (isset($_POST['submit'])) {
-    $name = $_POST['name'];
-    $email = $_POST['email'];
-    $number = $_POST['number'];
-    $files = $_FILES['files'];
-    // $mailTo = "info@showlandproducciones.es";
-    $mailTo = "gabrielcireslopez@gmail.com";
-            
-    $formcontent="Correo: $email \n\nNombre: $name \n\nTelf: $number \n\n$files";
+if($_POST && isset($_FILES['my_file']))
+{
 
-    $errorEmpty = false;
-    $errorEmail = false;
+    $recipient_email    = 'gabrielcireslopez@gmail.com'; //recipient email (most cases it is your personal email)
+    
+    //Capture POST data from HTML form and Sanitize them, 
+    $sender_name    = filter_var($_POST["sender_name"], FILTER_SANITIZE_STRING); //sender name
+    $reply_to_email = filter_var($_POST["sender_email"], FILTER_SANITIZE_STRING); //sender email used in "reply-to" header
+    $sender_number        = filter_var($_POST["sender_number"], FILTER_SANITIZE_STRING); //get subject from HTML form
 
-    if (empty($name) || empty($email) || empty($number) || empty($files)) {
-        echo "<span class='form-error'>Rellena los campos obligatorios!*</span>";
-        $errorEmpty = true;
+    
+    /* //don't forget to validate empty fields 
+    if(strlen($sender_name)<1){
+        die('Name is too short or empty!');
+    } 
+    */
+    
+    //Get uploaded file data
+    $file_tmp_name    = $_FILES['my_file']['tmp_name'];
+    $file_name        = $_FILES['my_file']['name'];
+    $file_size        = $_FILES['my_file']['size'];
+    $file_type        = $_FILES['my_file']['type'];
+    $file_error       = $_FILES['my_file']['error'];
 
-    } elseif (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
-        echo "<span class='form-error'>Introduce un correo válido!</span>";
-        $errorEmail = true;
+    if($file_error > 0)
+    {
+        die('Upload error or No files uploaded');
+    }
+    //read from the uploaded file & base64_encode content for the mail
+    $handle = fopen($file_tmp_name, "r");
+    $content = fread($handle, $file_size);
+    fclose($handle);
+    $encoded_content = chunk_split(base64_encode($content));
 
-    } else { 
-        mail($mailTo, $email, $formcontent);
-        echo "<span class='form-success'>!Correo enviado! Te responderemos cuanto antes.</span>";
+        $boundary = md5("sanwebe");
+        //header
+        $headers = "MIME-Version: 1.0\r\n"; 
+        $headers .= "From:".$reply_to_email."\r\n"; 
+        $headers .= "Reply-To: ".$reply_to_email."" . "\r\n";
+        $headers .= "Content-Type: multipart/mixed; boundary = $boundary\r\n\r\n"; 
+        
+        //plain text 
+        $body = "--$boundary\r\n";
+        $body .= "Content-Type: text/plain; charset=ISO-8859-1\r\n";
+        $body .= "Content-Transfer-Encoding: base64\r\n\r\n"; 
+        $body .= chunk_split(base64_encode($message)); 
+        
+        //attachment
+        $body .= "--$boundary\r\n";
+        $body .="Content-Type: $file_type; name=".$file_name."\r\n";
+        $body .="Content-Disposition: attachment; filename=".$file_name."\r\n";
+        $body .="Content-Transfer-Encoding: base64\r\n";
+        $body .="X-Attachment-Id: ".rand(1000,99999)."\r\n\r\n"; 
+        $body .= $encoded_content; 
+    
+    $sentMail = @mail($recipient_email, $subject, $body, $headers);
+    if($sentMail) //output success or failure messages
+    {       
+        die('Thank you for your email');
+    }else{
+        die('Could not send mail! Please check your PHP mail configuration.');  
     }
 
 }
-
 ?>
 
 <script>
