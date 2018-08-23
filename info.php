cf9b694886f80fe779f6f941269bdbde269e0d95
Gabriel Cires <gabrielcireslopez@gmail.com>
2018-08-23 12:42:16 +0200

    Problema al mandar archivos, resto de formulario funciona bien junto con las validaciones de js

--- info.php
+++ info.php
@@ -2,29 +2,34 @@
 
 if($_POST && isset($_FILES['my_file']) || isset($_POST['submit_form']))
 {
+
     //Capture POST data from HTML form and Sanitize them, 
     $sender_name    = filter_var($_POST["sender_name"], FILTER_SANITIZE_STRING); //sender name
-    // $sender_email = filter_var($_POST["sender_email"], FILTER_SANITIZE_STRING); //sender email used in "reply-to" header
-    $sender_email = filter_var($_POST["sender_email"], FILTER_VALIDATE_EMAIL); //sender email used in "reply-to" header
+    $sender_email = filter_var($_POST["sender_email"], FILTER_SANITIZE_STRING); //sender email used in "reply-to" header
     $sender_number        = filter_var($_POST["sender_number"], FILTER_SANITIZE_STRING); //get subject from HTML form
     $subject = "Formulario de Información y Presupuestos";
 
     $recipient_email    = 'gabrielcireslopez@gmail.com'; //recipient email (most cases it is your personal email)
 
     //Get uploaded file data
+    // $my_file          = $_FILES['my_file'];
     $file_tmp_name    = $_FILES['my_file']['tmp_name'];
     $file_name        = $_FILES['my_file']['name'];
     $file_size        = $_FILES['my_file']['size'];
     $file_type        = $_FILES['my_file']['type'];
     $file_error       = $_FILES['my_file']['error'];
 
+    strtolower(end(explode('.',$file_name)));
+    $formcontent="Nombre: $sender_name \n\nTelf: $sender_number";
+
     //read from the uploaded file & base64_encode content for the mail
     $handle = fopen($file_tmp_name, "r");
     $content = fread($handle, $file_size);
     fclose($handle);
     $encoded_content = chunk_split(base64_encode($content));
-
+    
         $boundary = md5("sanwebe");
+
         //header
         $headers = "MIME-Version: 1.0\r\n"; 
         $headers .= "From:".$sender_email."\r\n"; 
@@ -32,10 +37,10 @@ if($_POST && isset($_FILES['my_file']) || isset($_POST['submit_form']))
         $headers .= "Content-Type: multipart/mixed; boundary = $boundary\r\n\r\n"; 
 
         //plain text 
-        $body = "--$boundary\r\n";
+        $body .= "--$boundary\r\n";
         $body .= "Content-Type: text/plain; charset=ISO-8859-1\r\n";
         $body .= "Content-Transfer-Encoding: base64\r\n\r\n"; 
-        $body .= chunk_split(base64_encode($message)); 
+        $body .= chunk_split(base64_encode($formcontent));
 
         //attachment
         $body .= "--$boundary\r\n";
@@ -44,9 +49,7 @@ if($_POST && isset($_FILES['my_file']) || isset($_POST['submit_form']))
         $body .="Content-Transfer-Encoding: base64\r\n";
         $body .="X-Attachment-Id: ".rand(1000,99999)."\r\n\r\n"; 
         $body .= $encoded_content; 
-        $body .= $formcontent; 
-
-    $formcontent="Nombre: $sender_name \n\nTelf: $sender_number";
+        
 
     $errorEmpty = false;
     $errorEmail = false;
@@ -81,7 +84,7 @@ if($_POST && isset($_FILES['my_file']) || isset($_POST['submit_form']))
         $(".mailEmail").removeClass("all-good");
     }
     if (errorEmpty == false && errorEmail == false) {
-        $(".mailName, .mailEmail, .mailNumber, .mailFiles").val("");
+        $(".mailName, .mailEmail, .mailNumber").val("");
     }
 
 </script>
